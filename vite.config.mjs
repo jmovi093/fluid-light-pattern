ef01fb659a82910af68e09cebd650c49f2e45749
Josue <josue.montero.villalobos@est.una.ac.cr>
2026-01-30 16:06:33 -0600

    Auto-inject CSS using vite-plugin-css-injected-by-js

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import { resolve } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin(), // ✨ Inyecta CSS automáticamente
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
